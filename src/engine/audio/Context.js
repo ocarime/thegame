3e2c351ff04b785ef3e6ac144ac0aea650be0488
Danae Dekker <info@danaedekker.com>
2019-11-08 17:53:48 +0100

    Added AudioContext

--- src/engine/audio/Context.js
+++ src/engine/audio/Context.js
@@ -1,8 +1,10 @@
-import AudioClip from './AudioClip.js';
-
-
 // Class that defines the audio context
 export default class Context
 {
-  
+  // Constructor
+  constructor()
+  {
+    // Create the web audio context
+    this.webAudioContext = new AudioContext;
+  }
 }
